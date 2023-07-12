d75bdb31253e68212476080ff49daa3be15c1d58
yanglifeng <631676736@qq.com>
2023-07-12 17:04:20 +0800

    基因库-图件绘制

---
+++ src/components/common/ExternalIframe.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe style="height: 100%; width: 100%" :src="src" frameborder="0"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'ExternalIframe',
+  props: {
+    src: {
+      required: true,
+      type: String
+    }
+  },
+  beforeCreate() {
+    if (!this.src) {
+      this.$message.error('路径有误');
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
