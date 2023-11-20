1e599075ecf2b05c4b0a365aca2f4ebbc5ba4939
yanglifeng <631676736@qq.com>
2023-11-20 18:06:02 +0800

    新增 台账生成

--- src/components/common/ExternalIframe.vue
+++ src/components/common/ExternalIframe.vue
@@ -1,5 +1,6 @@
 <template>
-  <iframe style="height: 100%; width: 100%" :src="src" frameborder="0"></iframe>
+  <iframe style="height: 100%; width: 100%" :src="src" frameborder="0">
+  </iframe>
 </template>
 
 <script>
@@ -9,11 +10,6 @@ export default {
     src: {
       required: true,
       type: String
-    }
-  },
-  beforeCreate() {
-    if (!this.src) {
-      this.$message.error('路径有误');
     }
   }
 };
