34fa9e2e9c79a208c30b372cc372ce12b98ed17e
yanglifeng <631676736@qq.com>
2023-12-05 14:03:01 +0800

    feat：封装onlyoffice组件

---
+++ src/components/office/OfficeEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="height: 100%; overflow: auto"></div>
+</template>
+<script>
+export default {
+  name: 'OfficeEdit',
+  props: {
+    editorConfig: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      id: 'office-editor',
+      editor: null
+    };
+  },
+  mounted() {
+    this.initEditor();
+  },
+  methods: {
+    initEditor() {
+      // 初始化时清空 创建宿主元素
+      this.$el.innerHTML = '';
+      const el = document.createElement('div');
+      el.setAttribute('id', this.id);
+      this.$el.appendChild(el);
+      this.$nextTick(() => {
+        this.editor = new DocsAPI.DocEditor(this.id, this.editorConfig);
+      });
+    }
+  }
+};
+</script>
+<style lang=""></style>
---
+++ src/components/office/OfficeEditDialog.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-dialog
+    v-dialog-drag
+    class="office-edit"
+    :title="officeConf?.document?.title ?? ''"
+    :visible="visible"
+    :destroy-on-close="false"
+    :close-on-click-modal="false"
+    width="80%"
+    @close="handleClose"
+  >
+    <OfficeEdit :key="key" :editorConfig="officeConf" />
+  </el-dialog>
+</template>
+<script>
+import OfficeEdit from './OfficeEdit.vue';
+import { randomLenNum } from '@/util';
+
+export default {
+  name: 'OfficeEditDialog',
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    officeConf: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  components: { OfficeEdit },
+  data() {
+    return {
+      key: randomLenNum()
+    };
+  },
+  watch: {
+    visible(val) {
+      if (val) {
+        this.key = randomLenNum();
+      }
+    }
+  },
+  methods: {
+    handleClose() {
+      this.$emit('update:visible', false);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.office-edit ::v-deep {
+  .el-dialog .el-dialog__body {
+    height: calc(100vh - 165px - 54px - 40px);
+  }
+}
+</style>
